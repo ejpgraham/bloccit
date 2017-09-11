3c7dac17874fe35c7acbee568d7d3ea667282221
ejpgraham <ejpgraham@gmail.com>
2017-09-11 18:04:59 -0400

    Add logic to display each 5th title as SPAM

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,15 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    i = 1
+    @posts.each do |post|
+      post.title = "SPAM" if i % 5 == 0
+      i+= 1
+    end
   end
 
   def show
-    
+
   end
 
   def new
