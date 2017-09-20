23589e9bbadcb2fcd241b74c921a34b7a4dbd9d7
ejpgraham <ejpgraham@gmail.com>
2017-09-20 18:06:09 -0400

    Refactored specs.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,6 +21,10 @@ RSpec.describe User, type: :model do
     it "should have email and name attributes" do
       expect(user).to have_attributes(name: "Bloccit User", email: "user@bloccit.com")
     end
+
+    it "should use a callback to capitalize first and last name" do
+      expect(lowercase_user.name).to eq("Bloccit User")
+    end
   end
 
   describe "invalid user" do
