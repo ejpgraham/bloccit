2217d460aabdede94b2824242727a0a7fbcabf04
ejpgraham <ejpgraham@gmail.com>
2017-09-22 00:07:30 -0400

    Add confirmation view and route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
     resources :posts, except: [:index]
   end
 
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :post]
+
+  post 'users/confirm' => 'users#confirm'
 
   get 'about' => 'welcome#about'
 
