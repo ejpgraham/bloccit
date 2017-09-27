dcf45476b6279058a45e45c5248c34ae26bcd96a
ejpgraham <ejpgraham@gmail.com>
2017-09-27 16:29:39 -0400

    Add moderators that can edit posts and topics but not delete them.

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,7 +1,8 @@
 module PostsHelper
 
   def user_is_authorized_for_posts?(post)
-    current_user && (current_user.admin? || post.user == current_user)
+    current_user && (current_user.admin? || post.user == current_user || current_user.moderator?)
   end
-  
+
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -49,6 +49,13 @@ member = User.create!(
   password: "password",
 )
 
+moderator = User.create!(
+  name: "Moderator User",
+  email: "mod@example.com",
+  password: "password",
+  role: "moderator",
+)
+
 puts "Seed finished"
 puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
